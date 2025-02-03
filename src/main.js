898e303de4ac21fdb189c96f47ed80be6a4fc992
ZenithBuilds <zenithbuildsdevelopers@proton.me>
2025-02-03 17:19:46 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from './Home.js';
+import Home from './pages/Home.js';
 
 const routes = [
   { path: '/', component: Home }
