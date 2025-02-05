4fadc0806bcaa622d26dff34c825fd12e6aee41a
ZenithBuilds <zenithbuildsdevelopers@proton.me>
2025-02-04 20:52:48 -0600

    add some stuff

---
+++ src/components/loading.js
@@ -0,0 +1,11 @@
+export default {
+    template: 
+    `
+    <div class="loading-container">
+    <img class="loading" src="assets/icon.png"></img>
+    </div>
+    <div class="loadingtext-container">
+    <p class="loadingtext">Loading...</p>
+    </div>
+    `,
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,21 @@
+import Loading from '../components/loading.js';
+
 export default {
+    components: { Loading },
     template: `
-        <main>
+        <main v-if="loading">
+            <Loading/>
         </main>
-    `
+        <main v-else class="home-page">
+            <div class="">
+                <h1></h1>
+            </div>
+        </main>
+    `,
+    data: () => ({
+        loading: true,
+    }),
+    async mounted() {
+        this.loading = false;
+    },
 }
