aa13c7b562f05d3d642c52c2aa6aac14b1e6d885
ZenithBuilds <zenithbuildsdevelopers@proton.me>
2025-02-13 10:21:46 -0600

    Add some more stuff, just in case we lose data.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,9 @@ export default {
             <Loading/>
         </main>
         <main v-else class="home-page">
-            <div class="">
-                <h1></h1>
+            <div class="whyprebuilt-container">
+                <h1 class="home-title">Why pre-built?</h1>
+                <p class="type-label-desc" class="italic" id="whyprebuiltdesc">Building a computer is a whole hobby on it's own, and<br>you always have the risk of breaking something.<br>Buying from Zenith Builds you also get support, warranty,<br>and top of the line components with the cheapest pricing.</p>
             </div>
         </main>
     `,
